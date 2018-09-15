c136df97487d6a3ff8c655c8799b0b1eedff5964
stevezieglerva <stephen.v.ziegler@gmail.com>
2018-09-15 19:11:13 -0400

    draft files

---
+++ lambda_function.py
@@ -0,0 +1,22 @@
+import boto3
+from urllib.parse import *
+import json
+import datetime
+import time
+## from cache import *
+from ESLambdaLog import *
+from LocalTime import *
+## from LinkCheckResult import *
+## from Page import *
+## from Link import *
+## import sys
+import os
+import traceback
+import logging
+import structlog
+
+
+def lambda_handler(event, context):
+	return "Success"
+
+
