2702bd3aa8b66bf2d70049c3d12899546202ce71
stevezieglerva <stephen.v.ziegler@gmail.com>
2018-09-16 09:56:58 -0400

    Update test via s3 sycn process
    Full integration test

---
+++ aws-s3-to-es-bucket/full_integration_test_s3_sync.bat
@@ -0,0 +1,12 @@
+REM Does full integration test by creating a new S3 file. Results can be checked in CloudWatch logs
+
+
+set timestamp=%date:~10,4%-%date:~4,2%-%date:~7,2%-%time:~0,2%%time:~3,2%%time:~6,2%
+REM Replace spaces with underscores
+set timestamp=%timestamp: =_%
+
+
+copy /y test_s3.txt ".\ping_tests\%timestamp%.txt"
+call aws s3 sync  . s3://aws-s3-to-es/ --delete
+
+
--- aws-s3-to-es-bucket/sync.bat
+++
@@ -1,1 +0,0 @@
-call aws s3 sync  . s3://aws-s3-to-es/ --delete
